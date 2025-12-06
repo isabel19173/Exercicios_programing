7a73b6441ccfc3127b44d444c60dd24e86881e3c
Isabelle Kanzler <isabellekanzler@gmail.com>
2025-12-06 17:05:44 -0300

    tre

---
+++ javascript/for_exemplo.js
@@ -0,0 +1,8 @@
+let i= Number(prompt("Digite um número:"))
+//inicio ; verificação; passo/incremento
+const num = Number(prompt("Digite um número:"))
+for(i = 0; i <= 10; i++){//i=i+1;i+=1
+    //aqui vem o que precisa repetir
+  alert(i+"X" +num + "=" +(num*i))
+
+}
---
+++ javascript/isa.js
@@ -0,0 +1,11 @@
+//entrada
+let num;
+//faça(do) enquanto(while) é verdade
+do{
+  num= Number(prompt("Digite um número"))
+} while( num !=7);
+
+alert("Parabéns você acertou  :) o número digitado foi: "+num)
+
+//processo
+//saída
