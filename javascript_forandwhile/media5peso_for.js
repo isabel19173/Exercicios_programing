6bd3cb171e7c145697ef602f62ad07434d50409a
Isabelle Kanzler <isabellekanzler@gmail.com>
2025-12-11 19:05:53 -0300

    Update media5peso_for.js

--- javascript_forandwhile/media5peso_for.js
+++ javascript_forandwhile/media5peso_for.js
@@ -2,8 +2,8 @@ alert("A média de 5 pesos:")
 let soma = 0
 for (let i = 1; i <= 5; i++) {
     let peso = Number(prompt("Digite o peso " + i + ":" ))
-    soma += peso
+    soma = soma + peso
 }
-let media = soma / 5;
+let media = soma / 5
 alert("A média dos 5 pesos é: " + media.toFixed(2))
 
