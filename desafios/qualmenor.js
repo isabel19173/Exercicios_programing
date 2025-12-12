d59e1e19e16f139ad235a3ebad6164fff13fe539
Isabelle Kanzler <isabellekanzler@gmail.com>
2025-12-12 20:02:30 -0300

    concerto

--- desafios/qualmenor.js
+++ desafios/qualmenor.js
@@ -1,16 +1,15 @@
 //DESAFIO 03. Peça três números e diga qual é o menor.
 
-var num = Number(prompt('Informa qual é menor número: \n Digite um número:'))
-var num1= Number(prompt('Digite outro número:'))
-var num2= Number(prompt('Digite outro número:'))
+let num = Number(prompt('Informa qual é menor número: \n Digite um número:'))
+let num1= Number(prompt('Digite outro número:'))
+let num2= Number(prompt('Digite outro número:'))
 
-if(num > num1 && num > num2){
-    alert("O primeiro número : "+num+"; É menor do que segundo número: " +num1+" terceiro número:  "+num2)
-    
+if(num <= num1 && num <= num2){
+    alert("O primeiro número : "+num+"; É menor do que segundo número: " +num1+" e terceiro número:  "+num2)
 }
-else if(num1< num && num1< num2){
-    alert("O  segundo número : "+num1+"; É menor do que primeiro  número: "+num+" terceiro número:  "+num2)
+else if(num1<= num && num1<= num2){
+    alert("O  segundo número : "+num1+"; É menor do que primeiro  número: "+num+" e terceiro número:  "+num2)
 }
-else if(num2< num && num2< num1){
-    alert("O  terceiro número : "+num2+"; É menor do que primeiro  número: "+num+" segundo número:  "+num1)
+else {
+    alert("O  terceiro número : "+num2+"; É menor do que primeiro  número: "+num+" e segundo número:  "+num1)
 }
--- javascript_condi.js/qualmaior.js
+++ javascript_condi.js/qualmaior.js
@@ -1,7 +1,7 @@
 //15. Informar qual dos dois números é o maior
 
-var num = Number(prompt('Informa qual é maior número: \n Digite um número:'))
-var num1= Number(prompt('Digite outro número:'))
+let num = Number(prompt('Informa qual é maior número entre dois números : \n Digite um número:'))
+let num1= Number(prompt('Digite outro número:'))
 
 if(num < num1){
     alert("O  segundo número : "+num1+"; É maior do que primeiro  número: "+num)
